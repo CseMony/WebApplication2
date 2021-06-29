509374b8f40fcad9963eb597b7f7bf2cfdc40bac
mony <mony.bd@asa-international.com>
2021-06-29 22:20:41 +0600

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebApplication2;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { //Arrange:Initialize objects. Creates an instance of the class
+            Class1 tp = new Class1();
+            //assert part
+            Assert.AreEqual(tp.Name, "Unit Testing");
+        }
+    }
+}
---
+++ WebApplication2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class Class1
+    {
+        public String Name;
+        public Class1()
+        {
+            Name = "Unit Testing";
+        }
+    }
+}
